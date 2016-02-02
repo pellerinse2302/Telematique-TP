405916d3e7eb7eba01ae5e52870333b7420fc7e6
MicLamb <michael.o.lambert@gmail.com>
2016-02-02 17:17:53 -0500

    Added Extension class to have global functions

---
+++ TP1/Tp1/Tp1/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Tp1
+{
+    static class Extensions
+    {
+        public static byte[] Combine(params byte[][] arrays)
+        {
+            byte[] ret = new byte[arrays.Sum(x => x.Length)];
+            int offset = 0;
+            foreach (byte[] data in arrays)
+            {
+                Buffer.BlockCopy(data, 0, ret, offset, data.Length);
+                offset += data.Length;
+            }
+            return ret;
+        }
+
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+}
